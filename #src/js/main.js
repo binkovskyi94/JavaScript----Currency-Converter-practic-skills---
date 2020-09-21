b572971574343a3272c75c785712327dea433440
binkovskyi94 <guidekinoguidekino@gmail.com>
2020-09-21 23:03:55 +0300

    finish project

--- #src/js/main.js
+++ #src/js/main.js
@@ -10,14 +10,6 @@ inputRub.addEventListener('input', () => {
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
-    // request.addEventListener('readystatechange', () => {
-    //     if (request.readyState === 4 && request.status === 200) {
-    //         console.log(request.response);
-    //         const data = JSON.parse(request.response);
-    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-    //     } else {
-    //         inputUsd.value = "Что - то пошло не так";
-    //     }
     request.addEventListener('load', () => {
         if (request.status === 200) {
             const data = JSON.parse(request.response);
@@ -26,12 +18,5 @@ inputRub.addEventListener('input', () => {
             inputUsd.value = "Что - то пошло не так";
         }
     });
-
-    //status
-    //statusText
-    //response
-    //readyState
-
-
 
 });
